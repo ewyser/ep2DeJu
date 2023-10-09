e53487683bc503ea248794a23d09e6b35be2f4ce
ewyser <manuwyser@gmail.com>
2023-10-09 19:50:32 +0200

    cosmetics in ``solve()`` again

--- src/fun_fs/solve.jl
+++ src/fun_fs/solve.jl
@@ -1,4 +1,5 @@
 @views function solve!(meD,Δt)
+    # viscous damping
     η   = 0.1
     # initialize
     meD.f .= 0.0
@@ -7,12 +8,12 @@
     # solve momentum equation on the mesh
     @threads for n ∈ 1:meD.nno[meD.nD+1]
         if meD.m[n]>0.0 
-            m         = fill(1.0/meD.m[n],meD.nD) #(2,)
-            f         = meD.fext[n,:].-meD.fint[n,:]    #(2,)
-            D         = η.*sqrt(f[1]^2+f[2]^2).*sign.(meD.p[n,:].*m)      #()
-            f         = f.- D      #(2,)
-            meD.a[n,:] .= f.*m.*meD.bc[n,:]
-            meD.v[n,:] .= (meD.p[n,:].+Δt.*f).*m.*meD.bc[n,:]
+            m           = fill(1.0/meD.m[n],meD.nD)                     #(2,)
+            f           = meD.fext[n,:].-meD.fint[n,:]                  #(2,)
+            D           = η.*sqrt(f[1]^2+f[2]^2).*sign.(meD.p[n,:].*m)  #(2,)
+            f           = f.-D                                          #(2,)
+            meD.a[n,:] .= f.*m.*meD.bc[n,:]                             #(2,)
+            meD.v[n,:] .= (meD.p[n,:].+Δt.*f).*m.*meD.bc[n,:]           #(2,)
         end
     end
     return nothing
