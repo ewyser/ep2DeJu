fd375d85e31666b4edf34fe3a2eecc4af7e5854b
ewyser <manuwyser@gmail.com>
2023-10-09 19:44:29 +0200

    cosmetics in ``solve()``

--- src/fun_fs/solve.jl
+++ src/fun_fs/solve.jl
@@ -1,5 +1,5 @@
 @views function solve!(meD,Δt)
-    D   = 0.1
+    η   = 0.1
     # initialize
     meD.f .= 0.0
     meD.a .= 0.0
@@ -7,13 +7,12 @@
     # solve momentum equation on the mesh
     @threads for n ∈ 1:meD.nno[meD.nD+1]
         if meD.m[n]>0.0 
-            mnT          = fill(1.0/meD.m[n],meD.nD) #(2,)
-            fnT          = meD.fext[n,:].-meD.fint[n,:]    #(2,)
-            vnT          = meD.p[n,:] .*mnT         #(2,)
-            η            = sqrt(fnT[1]^2+fnT[2]^2)      #()
-            fnT          = fnT .- D.*η.*sign.(vnT)      #(2,)
-            meD.a[n,:] .= fnT.*mnT.*meD.bc[n,:]
-            meD.v[n,:] .= (meD.p[n,:].+Δt.*fnT).*mnT.*meD.bc[n,:]
+            m         = fill(1.0/meD.m[n],meD.nD) #(2,)
+            f         = meD.fext[n,:].-meD.fint[n,:]    #(2,)
+            D         = η.*sqrt(f[1]^2+f[2]^2).*sign.(meD.p[n,:].*m)      #()
+            f         = f.- D      #(2,)
+            meD.a[n,:] .= f.*m.*meD.bc[n,:]
+            meD.v[n,:] .= (meD.p[n,:].+Δt.*f).*m.*meD.bc[n,:]
         end
     end
     return nothing
