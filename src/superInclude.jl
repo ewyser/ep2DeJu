1f7106b453df4a08b722409e91f45e09664a92f9
ewyser <manuwyser@gmail.com>
2023-10-11 08:41:10 +0200

    add ``doc.jl``+ code cleaning

--- src/superInclude.jl
+++ src/superInclude.jl
@@ -1,11 +1,15 @@
 # include dependencies & function call(s) for svSolver.jl
 using Printf, LinearAlgebra, DelimitedFiles, Plots, LaTeXStrings, Base.Threads,ProgressMeter
-
+# include doc
+include("./misc/doc.jl")
+# include init
+include("./misc/functionsT.jl")
+include("./misc/RFS.jl")
+include("./misc/GRFS_gauss.jl")
+# include core functions
 include("./fun_fs/shpfun.jl")
 include("./fun_fs/mapsto.jl")
 include("./fun_fs/solve.jl")
 include("./fun_fs/elastoplast.jl")
 include("./fun_fs/plast.jl")
-include("./misc/functionsT.jl")
-include("./misc/RFS.jl")
-include("./misc/GRFS_gauss.jl")
+
