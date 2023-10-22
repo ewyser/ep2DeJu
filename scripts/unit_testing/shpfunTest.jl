4e55c36b48db6acb75e7fb8eb240327a90e06535
ewyser <manuwyser@gmail.com>
2023-10-22 22:12:45 +0200

    add PoU, see https://github.com/ewyser/ep2DeJu/issues/10

--- scripts/unit_testing/shpfunTest.jl
+++ scripts/unit_testing/shpfunTest.jl
@@ -16,6 +16,7 @@ include("../../src/superInclude.jl")
     dx = abs(xn[1]-xn[2])
     xB = vec(hcat(xn[3],xn[end-2]))
     a  = zeros(Float64,length(xp),length(xn),5)
+    PoU = zeros(Float64,length(xp))
     @info "shape functions calculation(s)..."
     if ϕ∂ϕType == "bsmpm"
         for mp ∈ eachindex(xp)
@@ -25,9 +26,15 @@ include("../../src/superInclude.jl")
                 type   = whichType(xn[nn],xB,dx)
                     a[mp,nn,end] = 1.0*type 
                 ϕx,dϕx = ϕ∇ϕ(ξ,type,dx)
+                if type != 0
+                    PoU[mp]+= ϕx
+                end
+
+
                 η      = (xp[mp] - xn[nn])/dx
                 type   = whichType(xn[nn],xB,dx)
                 ϕz,dϕz = ϕ∇ϕ(η,type,dx)
+
                 # convolution of basis function
                 a[mp,nn,1] = ϕx
                 a[mp,nn,2] = dϕx
@@ -71,21 +78,24 @@ include("../../src/superInclude.jl")
     scatter(x,y,zcolor=c,markershape=:circle,label="",show=true,aspect_ratio=1,cmap=cgrad(CM,4;categorical=true),markerstrokecolor=:auto,markerstrokewidth=0)
     scatter!(xn,zeros(size(xn)),color="red",markersize=5,xlabel=L"$x$ [m]",ylabel=L"\phi_n(x_p)",markershape=:square,label="",show=true,aspect_ratio=1,c=:viridis,markerstrokecolor=:auto,markerstrokewidth=0,xlim=(xn[3]-dx/8,xn[end-2]+dx/8),ylim=(-2,2),colorbar_title="type",levels=5,title=L"$\log_{10}(\phi_n(x_p)) > $"*string(log10(tol)))
     sleep(2.5)
-    savefig(path_plot*"check_$(ϕ∂ϕType)_Nsplineplot.png")
+    savefig(path_plot*"check_$(ϕ∂ϕType)_ϕ.png")
     
-    x = vec(a[:,:,4])
-    x = x[p]
+
     y = vec(a[:,:,2])
     y = y[p]
-    c = vec(a[:,:,end])
-    c = c[p]
 
     gr(size=(2.0*250,2*125),legend=true,markersize=2.25,markerstrokecolor=:auto)
     scatter(x,y,zcolor=c,markershape=:circle,label="",show=true,aspect_ratio=1,cmap=cgrad(CM,4;categorical=true),markerstrokecolor=:auto,markerstrokewidth=0)
     scatter!(xn,zeros(size(xn)),color="red",markersize=5,xlabel=L"$x$ [m]",ylabel=L"\partial_x\phi_n(x_p)",markershape=:square,label="",show=true,aspect_ratio=1,c=:viridis,markerstrokecolor=:auto,markerstrokewidth=0,xlim=(xn[3]-dx/8,xn[end-2]+dx/8),ylim=(-2,2),colorbar_title="type",levels=5,title=L"$\log_{10}(\phi_n(x_p)) > $"*string(log10(tol)))
     sleep(2.5)
-    savefig(path_plot*"check_$(ϕ∂ϕType)_dNsplineplot.png")
+    savefig(path_plot*"check_$(ϕ∂ϕType)_∂ϕ.png")
     
+    gr(size=(2.0*250,2*125),legend=true,markersize=2.25,markerstrokecolor=:auto)
+    scatter(xp,PoU,zcolor=c,markershape=:circle,label="",show=true,aspect_ratio=1,cmap=cgrad(CM,4;categorical=true),markerstrokecolor=:auto,markerstrokewidth=0)
+    scatter!(xn,zeros(size(xn)),color="red",markersize=5,xlabel=L"$x$ [m]",ylabel=L"$\sum_n\phi_n(x_p)$",markershape=:square,label="",show=true,aspect_ratio=1,c=:viridis,markerstrokecolor=:auto,markerstrokewidth=0,xlim=(xn[3]-dx/8,xn[end-2]+dx/8),ylim=(-2,2),colorbar_title="type",levels=5,title="partition of unity")
+    sleep(2.5)
+    savefig(path_plot*"check_$(ϕ∂ϕType)_PoU.png")
+
     @info "Figs saved in" path_plot
     return println("[=> done! exiting...")
 end
