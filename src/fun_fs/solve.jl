2ac57b2f50189098bd66835b4ce5931e687cbe5f
ewyser <manuwyser@gmail.com>
2023-10-25 01:21:33 +0200

    add damping

--- src/fun_fs/solve.jl
+++ src/fun_fs/solve.jl
@@ -6,8 +6,10 @@
     # solve momentum equation on the mesh
     @threads for n ∈ 1:meD.nno[meD.nD+1]
         if meD.mn[n]>0.0 
-            m             = (1.0/meD.mn[n]).*meD.bc[n,:]                   #(2,) 
-            meD.vn[n,:] .+= (meD.pn[n,:].+Δt.*meD.oobf[n,:]).*m            #(2,)
+            m           = (1.0/meD.mn[n]).*meD.bc[n,:]                   #(2,)
+            meD.Dn[n,:].= η.*norm(meD.oobf[n,:]).*sign.(meD.pn[n,:].*m)  #(2,)
+            meD.fn[n,:].= meD.oobf[n,:].-meD.Dn[n,:]                     #(2,)
+            meD.vn[n,:].= (meD.pn[n,:].+Δt.*meD.fn[n,:]).*m              #(2,)
         end
     end
     return nothing
