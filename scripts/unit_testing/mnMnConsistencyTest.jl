924f80d40370bed7fb99a465d8786e03e4e34bb2
Manu Wyser <manuwyser@gmail.com>
2023-10-25 14:51:30 +0200

    add test for differences between lumped mass vector and lumped mass matrix vector

---
+++ scripts/unit_testing/mnMnConsistencyTest.jl
@@ -0,0 +1,60 @@
+# include("./scripts/unit_testing/mnMnConsistencyTest.jl")
+# include dependencies
+using Test
+include("../../src/misc/types.jl")
+include("../../src/misc/utilities.jl")
+include("../../src/misc/setup.jl")
+include("../../src/misc/physics.jl")
+include("../../src/misc/plot.jl")
+include("../../src/fun_fs/shpfun.jl")
+
+# main program
+@views function mnMn(nel::Int64,varPlot::String,cmType::String; kwargs...)
+    ϕ∂ϕType,fwrkDeform,trsfrAp,isΔFbar = getKwargs(kwargs)
+    @info "** ϵp2De v$(getVersion()): $(fwrkDeform) strain formulation **"
+    # independant physical constant
+    g       = 9.81                                                              # gravitationnal acceleration [m/s^2]            
+    K,G,Del = D(1.0e6,0.3)                                                      # elastic matrix D(E,ν) Young's mod. [Pa] + Poisson's ratio [-]    
+    ρ0      = 2700.0                                                            # density [kg/m^3]
+    yd      = sqrt((K+4.0/3.0*G)/ρ0)                                            # elastic wave speed [m/s]
+    c0,cr   = 20.0e3,4.0e3                                                      # cohesion [Pa]
+    ϕ0,ϕr,ψ0= 20.0*π/180,7.5*π/180,0.0                                          # friction angle [Rad], dilation angle [Rad]                                                              
+    t,te,tg = 15.0,10.0,15.0/1.5                                                # simulation time [s], elastic loading [s], gravity load
+    # mesh & mp setup
+    L       = [64.1584,12.80]                                                   # domain geometry
+    meD     = meshSetup(nel,L,typeD)                                            # mesh geometry setup
+    mpD     = pointSetup(meD,L,c0,cr,ϕ0,ϕr,ρ0,typeD)                            # material point geometry setup
+    Hp      = -60.0e3*meD.h[1]                                                  # softening modulus
+    # constitutive model param.
+    cmParam = (Kc = K, Gc = G, Del = Del, Hp = Hp,)
+    @info "mesh & mp feature(s):" dim=meD.nD nel=Int64(meD.nel[end]) nno=meD.nno[end] nmp=mpD.nmp
+    
+    # calculate shape functions
+    ϕ∂ϕ!(mpD,meD,ϕ∂ϕType)
+    # initialize nodal quantities
+    meD.Mn  .= 0.0
+    meD.mn  .= 0.0
+    # mapping back to mesh
+    @threads for dim ∈ 1:meD.nD
+        @simd for p ∈ 1:mpD.nmp
+            # accumulation
+            if dim == 1 
+                # lumped mass matrix
+                meD.mn[mpD.p2n[:,p]].+= mpD.ϕ∂ϕ[:,p,1].*mpD.m[p]
+                # consistent mass matrix
+                meD.Mn[mpD.p2n[:,p],mpD.p2n[:,p]].+= (mpD.ϕ∂ϕ[:,p,1].*mpD.ϕ∂ϕ[:,p,1]').*mpD.m[p] 
+            end
+        end
+    end
+    # lumped mass matrix and lumped mass vector
+    mn_lumped = sum(meD.Mn,dims=2)
+    mn        = meD.mn
+    return abs.(mn_lumped.-mn)
+end
+
+@testset "lumped consistent mass matrix vs. lumped mass vector:" begin
+    diff = maximum(mnMn(40,"P","MC"))
+    for (it,limit) in enumerate([1e-1,1e-2,1e-4,1e-8,1e-16])
+        @test diff < limit 
+    end
+end
