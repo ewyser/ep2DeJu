10bce7fc17c3e11a76963e33e848276aeee7a7c1
Manu Wyser <manuwyser@gmail.com>
2023-10-26 12:40:52 +0200

    add ``compactionTest.jl``, see https://github.com/ewyser/ep2DeJu/issues/11

---
+++ scripts/unit_testing/compactionTest.jl
@@ -0,0 +1,54 @@
+# include("./scripts/unit_testing/compactionTest.jl")
+# include dependencies
+include("../../src/superInclude.jl")
+# main program
+@views function compactTest(nel::Int64,varPlot::String,cmType::String; kwargs...)
+    ϕ∂ϕType,fwrkDeform,trsfrAp,isΔFbar = getKwargs(kwargs)
+    @info "** ϵp2De v$(getVersion()): compaction of a two-dimensional column under self weight **"
+    # independant physical constant
+    g       = 9.81                                                              # gravitationnal acceleration [m/s^2]            
+    K,G,Del = D(1.0e6,0.3)                                                      # elastic matrix D(E,ν) Young's mod. [Pa] + Poisson's ratio [-]    
+    ρ0      = 2700.0                                                            # density [kg/m^3]
+    yd      = sqrt((K+4.0/3.0*G)/ρ0)                                            # elastic wave speed [m/s]
+    c0,cr   = 20.0e3,4.0e3                                                      # cohesion [Pa]
+    ϕ0,ϕr,ψ0= 20.0*π/180,7.5*π/180,0.0                                          # friction angle [Rad], dilation angle [Rad]                                                              
+    t,te,tg = 10.0,10.0,10.0                                                    # simulation time [s], elastic loading [s], gravity load
+    # mesh & mp setup
+    L       = [10.0,10.0]                                                       # domain geometry
+    meD     = meshSetup(nel,L,typeD)                                            # mesh geometry setup
+    mpD     = pointSetup(meD,L,c0,cr,ϕ0,ϕr,ρ0,typeD)                            # material point geometry setup
+    Hp      = -60.0e3*meD.h[1]                                                  # softening modulus
+    # constitutive model param.
+    cmParam = (Kc = K, Gc = G, Del = Del, Hp = Hp,)
+    @info "mesh & mp feature(s):" dim=meD.nD nel=Int64(meD.nel[end]) nno=meD.nno[end] nmp=mpD.nmp
+    # plot & time stepping parameters
+    tw,tC,it,ctr,ηmax,ηtot = 0.0,1.0,0,0,0,0    
+    # action
+    @info "launch $(ϕ∂ϕType) calculation cycle..."
+    prog  = ProgressUnknown("working hard:", spinner=true,showspeed=true)
+    while tw<=t
+        # plot/save
+        if tw >= ctr*tC ctr = plotStuff(mpD,tw,varPlot,ctr) end
+        # set clock on/off
+        tic = time_ns()
+        # adaptative Δt & linear increase in gravity
+        Δt,g  = get_Δt(mpD.v,meD.h,yd),get_g(tw,tg,meD.nD)
+        # bsmpm cycle
+        ϕ∂ϕ!(mpD,meD,ϕ∂ϕType)
+        mapsto!(mpD,meD,g,Δt,trsfrAp,"p->n")                  
+        solve!(meD,Δt)
+        mapsto!(mpD,meD,g,Δt,trsfrAp,"p<-n")
+        ηmax = elastoplast!(mpD,meD,cmParam,cmType,Δt,isΔFbar,fwrkDeform,tw>te)
+        # update sim time
+        tw,it,toc,ηtot = tw+Δt,it+1,((time_ns()-tic)),max(ηmax,ηtot)
+        # update progress bas
+        next!(prog;showvalues = getVals(meD,mpD,it,ηmax,ηtot,tw/t,"(✗)"))
+    end
+    ProgressMeter.finish!(prog, spinner = '✓',showvalues = getVals(meD,mpD,it,ηmax,ηtot,1.0,"(✓)"))
+    ctr     = plotStuff(mpD,tw,varPlot,ctr)
+    sleep(2.5)
+    savefig(path_plot*"$(varPlot)_$(ϕ∂ϕType)_$(fwrkDeform)_$(isΔFbar)_$(cmType).png")
+    @info "Figs saved in" path_plot
+    return msg("(✓) Done! exiting...")
+end
+compactTest(40,"P","MC";shpfun=:bsmpm,fwrk=:finite,vollock=true)
