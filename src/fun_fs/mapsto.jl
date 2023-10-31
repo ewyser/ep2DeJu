c7e91be3b475b9a4a5e2e9d05f000b510e8be8ff
ewyser <manuwyser@gmail.com>
2023-10-31 01:29:20 +0100

    add ``apic`, see https://github.com/ewyser/ep2DeJu/issues/8. future work should evaluate implementation

--- src/fun_fs/mapsto.jl
+++ src/fun_fs/mapsto.jl
@@ -77,9 +77,40 @@ end
 end
 @views function apicMapping!(mpD,meD,g,Δt,mapsto)
     if mapsto == "p->n"
+        # initialize nodal quantities
+        meD.Mn  .= 0.0
+        meD.mn  .= 0.0
+        meD.pn  .= 0.0
+        meD.oobf.= 0.0
+        # mapping back to mesh
+        @threads for dim ∈ 1:meD.nD
+            @simd for p ∈ 1:mpD.nmp
+                # accumulation
+                if dim == 1 
+                    # lumped mass matrix
+                    meD.mn[mpD.p2n[:,p]].+= mpD.ϕ∂ϕ[:,p,1].*mpD.m[p]
+                    # consistent mass matrix
+                    meD.Mn[mpD.p2n[:,p],mpD.p2n[:,p]].+= (mpD.ϕ∂ϕ[:,p,1].*mpD.ϕ∂ϕ[:,p,1]').*mpD.m[p] 
+                end
+                δx= (meD.xn[mpD.p2n[:,p],:].-repeat(mpD.x[p,:]',meD.nn,1))'
+                A = mpD.v[p,:].+(mpD.Bapic[:,:,p]*inv(mpD.Dapic[:,:,p])*δx)
 
+                meD.pn[  mpD.p2n[:,p],dim].+= mpD.ϕ∂ϕ[:,p,1].*mpD.m[p].*A[dim,:]
+                meD.oobf[mpD.p2n[:,p],dim].+= mpD.ϕ∂ϕ[:,p,1].*(mpD.m[p]*g[dim]      )
+                meD.oobf[mpD.p2n[:,p],dim].-= mpD.V[p].*(mpD.B[dim:meD.nD:end,:,p]*mpD.σ[:,p])
+            end
+        end
+        # lumped mass matrix
+        #meD.mn .= sum(meD.Mn,dims=2)
     elseif mapsto == "p<-n"
-    
+        # mapping back to mp's
+        @simd for dim ∈ 1:meD.nD
+            @threads for p ∈ 1:mpD.nmp        
+                # pic update
+                mpD.v[p,dim] =    (mpD.ϕ∂ϕ[:,p,1]'*meD.vn[mpD.p2n[:,p],dim])
+                mpD.x[p,dim]+= Δt*(mpD.ϕ∂ϕ[:,p,1]'*meD.vn[mpD.p2n[:,p],dim])
+            end          
+        end
     end
     return nothing
 end
@@ -110,6 +141,28 @@ end
     end
     return nothing
 end
+@views function getB!(mpD,meD)
+    # initialize
+    mpD.Bapic.= 0.0
+    @threads for p ∈ 1:mpD.nmp
+        δx = (meD.xn[mpD.p2n[:,p],:].-repeat(mpD.x[p,:]',meD.nn,1))'
+        for nn ∈ 1:meD.nn
+            mpD.Bapic[:,:,p] .+= mpD.ϕ∂ϕ[nn,p,1].*meD.vn[mpD.p2n[nn,p],:]'.*δx[nn]
+        end
+    end  
+    return nothing
+end
+@views function getD!(mpD,meD)
+    # initialize
+    mpD.Dapic.= 0.0
+    @threads for p ∈ 1:mpD.nmp
+        for nn ∈ 1:meD.nn
+            δx = meD.xn[mpD.p2n[nn,p],:].-mpD.x[p,:]
+            mpD.Dapic[:,:,p].+= (δx'.*δx)
+        end
+    end  
+    return nothing
+end
 #--------------------------------------------------------------------------------------------
 ## dispatcher functions
 #--------------------------------------------------------------------------------------------
@@ -118,6 +171,9 @@ end
         flipMapping!(mpD,meD,g,Δt,whereto)
     elseif trsfrAp == :tpicUSL
         tpicMapping!(mpD,meD,g,Δt,whereto)
+    elseif trsfrAp == :apicUSL
+        apicD!(mpD,meD)
+        apicMapping!(mpD,meD,g,Δt,whereto)
     end
     return nothing
 end
@@ -127,6 +183,9 @@ end
         DM!(       mpD,meD,Δt)
     elseif trsfrAp == :tpicUSL
         tpicMapping!(mpD,meD,g,Δt,whereto)
+    elseif trsfrAp == :apicUSL
+        getB!(mpD,meD)
+        apicMapping!(mpD,meD,g,Δt,whereto)
     end
     return nothing
 end
