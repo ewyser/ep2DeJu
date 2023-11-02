962ceb5d3656383d1c8cafe5fde78f3f318be4d8
ewyser <manuwyser@gmail.com>
2023-11-03 00:04:13 +0100

    still fixing same issue

--- src/misc/plot.jl
+++ src/misc/plot.jl
@@ -12,15 +12,18 @@ default(
     )
 # plot routines
 @views function plot_coh(xp,coh,phi,coh0,ϕ0)
+    coh0 = coh0/1e3
     if size(xp,2)==2
         gr(size=(2.0*250,2*125),legend=true,markersize=2.25,markerstrokecolor=:auto)
         scatter(xp[:,1],xp[:,2],zcolor=coh./1e3,
         markershape=:circle,
         label="",
-        show=true,
+        xlabel = L"$x-$direction",
+        ylabel = L"$z-$direction",
+        title  = L"c_0(x_p)",
         aspect_ratio=1,
         c=:vik,
-        clims=(10.0,30.0),
+        clims=(coh0-coh0/2,coh0+coh0/2),
         markerstrokecolor=:auto,
         markerstrokewidth=0,
         ylim=(-10,20),
@@ -30,10 +33,12 @@ default(
         scatter(xp[:,1],xp[:,2],xp[:,3],zcolor=coh./1e3,
         markershape=:circle,
         label="",
-        show=true,
+        xlabel = L"$x-$direction",
+        ylabel = L"$z-$direction",
+        title  = L"c_0(x_p)",
         aspect_ratio=:equal,
         c=:vik,
-        clims=(10.0,30.0),
+        clims=(coh0-coh0/2,coh0+coh0/2),
         markerstrokecolor=:auto,
         markerstrokewidth=0,
         )
@@ -43,7 +48,9 @@ default(
     scatter(xp[:,1],xp[:,2],zcolor=phi,
     markershape=:circle,
     label="",
-    show=true,
+    xlabel = L"$x-$direction",
+    ylabel = L"$z-$direction",
+    title  = L"\phi_0(x_p)",
     aspect_ratio=:equal,
     c=:vik,
     clims=(ϕ0-ϕ0/5,ϕ0+ϕ0/5),
@@ -73,7 +80,7 @@ end
     end
     # plot
     gr(legend=true,markersize=2.5,markershape=:circle,markerstrokewidth=0.75,)#markerstrokecolor=:match,)
-    p1 = scatter(mpD.x[:,1],mpD.x[:,2],zcolor=d,
+    p1 = scatter(mpD.x[:,1],mpD.x[:,end],zcolor=d,
     xlabel = L"$x-$direction",
     ylabel = L"$z-$direction",
     label  = lab,
