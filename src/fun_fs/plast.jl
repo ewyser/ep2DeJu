2016453deb91ee9cbc0ef201acba9f8404d4cf5d
ewyser <manuwyser@gmail.com>
2023-11-06 15:49:48 +0100

    cosmetics in ``DPplast!( )``

--- src/fun_fs/plast.jl
+++ src/fun_fs/plast.jl
@@ -1,4 +1,4 @@
-@views function getPσ0τ0τII(σ,nstr)
+@views function getInitial(σ,nstr)
     σ0 = σ
     P  = (σ0[1]+σ0[2]+σ0[3])/3.0
     if nstr == 4
@@ -10,7 +10,7 @@
     end
     return P,σ0,τ0,τII
 end
-@views function getηηBξ(ϕ,ψ,c,nstr)
+@views function getDPParam(ϕ,ψ,c,nstr)
     if nstr == 4
         η   = 3.0*tan(ϕ)/(sqrt(9.0+12.0*tan(ϕ)*tan(ϕ)))
         ηB  = 3.0*tan(ψ)/(sqrt(9.0+12.0*tan(ψ)*tan(ψ)))
@@ -22,7 +22,7 @@ end
     end
     return η,ηB,ξ
 end
-@views function σNew!(Pn,τ0,τn,τII,nstr)
+@views function σn(Pn,τ0,τn,τII,nstr)
     if nstr == 4
         σ = τ0.*(τn/τII).+[Pn,Pn,Pn,0.0]
     elseif nstr == 6
@@ -42,8 +42,8 @@ end
     for p ∈ 1:mpD.nmp
         c   = mpD.c0[p]+cmParam.Hp*mpD.ϵpII[p]
         if c<mpD.cr[p] c = mpD.cr[p] end
-        P,σ0,τ0,τII = getPσ0τ0τII(σ[:,p],nstr)
-        η,ηB,ξ      = getηηBξ(mpD.ϕ[p],ψ,c,nstr)
+        P,σ0,τ0,τII = getInitial(σ[:,p],nstr)
+        η,ηB,ξ      = getDPParam(mpD.ϕ[p],ψ,c,nstr)
         σm,τP       = ξ/η,ξ-η*(ξ/η)
         fs,ft       = τII+η*P-ξ,P-σm         
         αP,h        = sqrt(1.0+η^2)-η,τII-τP-(sqrt(1.0+η^2))*(P-σm)  
@@ -51,12 +51,11 @@ end
             Δλ          = fs/(cmParam.Gc+cmParam.Kc*η*ηB)
             Pn,τn       = P-cmParam.Kc*ηB*Δλ,ξ-η*(P-cmParam.Kc*ηB*Δλ)
             σ[:,p]     .= σNew!(Pn,τ0,τn,τII,nstr)
-            ϵII         = Δλ*sqrt(1/3+2/9*ηB^2)
-            mpD.ϵpII[p]+= ϵII
-
-            ϵN = cmParam.Del\(σ0.-σ[:,p])
+            ϵpII        = Δλ*sqrt(1/3+2/9*ηB^2)
+            mpD.ϵpII[p]+= ϵpII
             if fwrkDeform == :finite
-                mpD.ϵ[:,:,p].-= mutate(ϵN,0.5,:tensor)
+                ϵn            = cmParam.Del\(σ0.-σ[:,p])
+                mpD.ϵ[:,:,p].-= mutate(ϵn,0.5,:tensor)
                 # update left cauchy green tensor
                 λ,n           = eigen(mpD.ϵ[:,:,p],sortby=nothing)
                 mpD.b[:,:,p] .= n*diagm(exp.(2.0.*λ))*n'
@@ -65,7 +64,7 @@ end
         if h<=0.0 && P>=σm
             Δλ          = (P-σm)/cmParam.Kc
             Pn          = σm-P
-            σ[:,p]     .= σNew!(Pn,τ0,0.0,τII,nstr)
+            σ[:,p]     .= σn(Pn,τ0,0.0,τII,nstr)
             Δϵ          = cmParam.Del\(σ0-σ[:,p])
             mpD.ϵpII[p]+= sqrt(2.0)*Δλ/3.0
         end
