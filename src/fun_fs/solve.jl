5026bc8c5c5ef0a8101e3eb83581931e9d233df1
ewyser <manuwyser@gmail.com>
2023-11-06 10:45:01 +0100

    reduced memory allocation

--- src/fun_fs/solve.jl
+++ src/fun_fs/solve.jl
@@ -6,13 +6,15 @@
     meD.an.= 0.0
     meD.vn.= 0.0
     # solve momentum equation on the mesh
-    @threads for n ∈ 1:meD.nno[meD.nD+1]
-        if meD.mn[n]>0.0 
-            m            = (1.0/meD.mn[n]).*meD.bc[n,:]                   #(2,)
-            meD.Dn[n,:] .= η.*norm(meD.oobf[n,:]).*sign.(meD.pn[n,:].*m)  #(2,)
-            meD.fn[n,:] .= meD.oobf[n,:].-meD.Dn[n,:]                     #(2,)
-            meD.an[n,:] .= meD.fn[n,:].*m                                 #(2,)
-            meD.vn[n,:] .= (meD.pn[n,:].+Δt.*meD.fn[n,:]).*m              #(2,)
+    for dim ∈ 1:meD.nD
+        @threads for n ∈ 1:meD.nno[meD.nD+1]
+            if meD.mn[n]>0.0 
+                m             = (1.0/meD.mn[n])*meD.bc[n,dim]                   #(2,)
+                meD.Dn[n,dim] = η*norm(meD.oobf[n,:])*sign(meD.pn[n,dim]*m)     #(2,)
+                meD.fn[n,dim] = meD.oobf[n,dim]-meD.Dn[n,dim]                   #(2,)
+                meD.an[n,dim] = meD.fn[n,dim]*m                                 #(2,)
+                meD.vn[n,dim] = (meD.pn[n,dim]+Δt*meD.fn[n,dim])*m              #(2,)
+            end
         end
     end
     return nothing
