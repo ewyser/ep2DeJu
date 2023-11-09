544242e79e38c49df7d6215ba29289abb846eb65
ewyser <manuwyser@gmail.com>
2023-11-10 00:08:25 +0100

    minor improvement in ``camCRetMap!( )``

--- src/fun_fs/RetMap/camCRetMap.jl
+++ src/fun_fs/RetMap/camCRetMap.jl
@@ -1,17 +1,30 @@
-@views function getCamCParam(σ0,nstr)
+@views function camCParam(σ0,χ,nstr)
     if nstr == 3
         P  = (σ0[1]+σ0[2])/2.0
         ξ  = σ0.-[P,P,0.0]
         J2 = 0.5*(ξ[1]^2+ξ[2]^2+2.0*ξ[3]^2) # Borja (2013), p.33
         ξn = sqrt(2.0*J2) 
+        n  = ξ./ξn
+        q  = sqrt(χ)*ξn
     elseif nstr == 6
         P  = (σ0[1]+σ0[2]+σ0[3])/3.0
         ξ  = σ0.-[P,P,P,0.0,0.0,0.0]
         J2 = 0.5*(ξ[1]^2+ξ[2]^2+ξ[3]^2+2.0*ξ[4]^2+2.0*ξ[5]^2+2.0*ξ[6]^2) # Borja (2013), p.33
         ξn = sqrt(2.0*J2) 
+        n  = ξ./ξn
+        q  = sqrt(χ)*ξn
     end
-    return P,ξ,ξn,J2
+    return P,q,n
 end
+@views function camCYield(P,q,Pt,a,β,M)
+    if P<(Pt-a) 
+        b = β 
+    else 
+        b = 1.0 
+    end
+    f = (1.0/b^2)*(P-Pt+a)^2+(q/M)^2-a^2 # De Souza Neto (2008)
+    return f,b
+end 
 @views function ∂f(∂f∂p,∂f∂q,n,χ,nstr)
     if nstr == 3
         ∂f∂σ = [∂f∂p*1.0/3.0+sqrt(χ)*∂f∂q*n[1];
@@ -43,11 +56,9 @@ end
     Ps = zeros(mpD.nmp)
     Qs = zeros(mpD.nmp)
     @threads for p in 1:mpD.nmp
-        ϕcs,a     = mpD.ϕ[p],a0*(exp(-ζ*mpD.ϵpV[p]))
-        P,ξ,ξn,J2 = getCamCParam(σ[:,p],nstr)
-        n,q       = ξ./ξn,sqrt(χ)*ξn
-        if P<(Pt-a) b = β else b = 1.0 end
-        f         = (1.0/b^2)*(P-Pt+a)^2+(q/M)^2-a^2 # De Souza Neto (2008)
+        ϕcs,a  = mpD.ϕ[p],a0*(exp(-ζ*mpD.ϵpV[p]))
+        P,q,n  = camCParam(σ[:,p],χ,nstr)
+        f,b    = camCYield(P,q,Pt,a,β,M)
         if f>0.0 
             σ0    = copy(σ[:,p])
             ϵpV,γ = mpD.ϵpV[p],mpD.ϵpII[p]
@@ -61,10 +72,8 @@ end
                 ϵpV += Δλ*∂f∂p
                 γ   += Δλ*∂f∂q
                 a    = a0*(exp(-ζ*ϵpV))
-                P,ξ,ξn,J2 = getCamCParam(σ0,nstr)
-                n,q       = ξ./ξn,sqrt(χ)*ξn
-                if P<(Pt-a) b = β else b = 1.0 end
-                f         = (1.0/b^2)*(P-Pt+a)^2+(q/M)^2-a^2 # De Souza Neto (2008)
+                P,q,n = camCParam(σ0,χ,nstr)
+                f,b   = camCYield(P,q,Pt,a,β,M)
                 η   +=1
             end
             mpD.ϵpV[p] = ϵpV
