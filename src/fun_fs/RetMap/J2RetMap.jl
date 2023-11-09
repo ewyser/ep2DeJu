97844b57f38c50c972e567b67951c69e19e05010
ewyser <manuwyser@gmail.com>
2023-11-09 23:41:33 +0100

    minor improvements in ``J2RetMap!( )``

--- src/fun_fs/RetMap/J2RetMap.jl
+++ src/fun_fs/RetMap/J2RetMap.jl
@@ -1,16 +1,23 @@
-@views function getInitial(σ0,nstr)
+@views function J2Param(σ0,χ,nstr)
     if nstr == 3
         P  = (σ0[1]+σ0[2])/2.0
         ξ  = σ0.-[P,P,0.0]
         J2 = 0.5*(ξ[1]^2+ξ[2]^2+2.0*ξ[3]^2) # Borja (2013), p.33
         ξn = sqrt(2.0*J2) 
+        n  = ξ./ξn
+        q  = sqrt(χ)*ξn
     elseif nstr == 6
         P  = (σ0[1]+σ0[2]+σ0[3])/3.0
         ξ  = σ0.-[P,P,P,0.0,0.0,0.0]
         J2 = 0.5*(ξ[1]^2+ξ[2]^2+ξ[3]^2+2.0*ξ[4]^2+2.0*ξ[5]^2+2.0*ξ[6]^2) # Borja (2013), p.33
         ξn = sqrt(2.0*J2) 
+        n  = ξ./ξn
+        q  = sqrt(χ)*ξn
     end
-    return ξ,ξn,J2
+    return P,q,n,ξn
+end
+@views function J2Yield(ξn,κ)
+    return f = ξn-κ
 end
 @views function J2RetMap!(mpD,cmParam,fwrkDeform) # Borja (1990); De Souza Neto (2008)
     ftol,ηtol,ηmax = 1e-9,1e4,20
@@ -22,23 +29,22 @@ end
         σ,nstr = mpD.σ,size(mpD.σ,1)
     end
     @threads for p in 1:mpD.nmp
+        P,q,n,ξn = J2Param(σ[:,p],χ,nstr)
         κ = 2.5*mpD.c0[p]+cmParam.Hp*mpD.ϵpII[p]
         if κ <= mpD.cr[p] κ = mpD.cr[p] end
-        ξ,ξn,J2 = getInitial(σ[:,p],nstr)
-        n,q,f   = ξ./ξn,sqrt(χ)*ξn,ξn-κ
+        f        = J2Yield(ξn,κ)
         if f>0.0 
-            γ0,σ0 = copy(mpD.ϵpII[p]),copy(σ[:,p])
-            ηit  = 1
+            γ0,σ0,ηit = copy(mpD.ϵpII[p]),copy(σ[:,p]),1
             while abs(f)>ftol && ηit<ηmax
-                ∂f∂σ    = n
-                Δλ      = f/(∂f∂σ'*cmParam.Del*∂f∂σ)
-                Δσ      = (Δλ*cmParam.Del*∂f∂σ)        
-                σ0    .-= Δσ 
-                γ0     += Δλ
-                ξ,ξn,J2 = getInitial(σ0,nstr)
-                κ       = 2.5*mpD.c0[p]+cmParam.Hp*mpD.ϵpII[p]
+                ∂f∂σ     = n
+                Δλ       = f/(∂f∂σ'*cmParam.Del*∂f∂σ)
+                Δσ       = (Δλ*cmParam.Del*∂f∂σ)        
+                σ0     .-= Δσ 
+                γ0      += Δλ
+                P,q,n,ξn = J2Param(σ0,χ,nstr)
+                κ        = 2.5*mpD.c0[p]+cmParam.Hp*mpD.ϵpII[p]
                 if κ <= mpD.cr[p] κ = mpD.cr[p] end
-                n,q,f   = ξ./ξn,sqrt(χ)*ξn,ξn-κ
+                f        = J2Yield(ξn,κ)
                 ηit +=1
             end
             mpD.ϵpII[p] = γ0
