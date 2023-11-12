7ed2cd762120fccb0704670e41c9730b97b62c15
ewyser <manuwyser@gmail.com>
2023-11-12 11:09:48 +0100

    add symmetric colorbar for volumetric plastic strain ``epV``

--- src/misc/plot.jl
+++ src/misc/plot.jl
@@ -70,19 +70,31 @@ end
             d   = -(mpD.σ[1,:]+mpD.σ[2,:]+mpD.σ[3,:])/3/1e3
             lab = L"$p=-\left(\sigma_{xx,p}+\sigma_{yy,p}+\sigma_{zz,p}\right)/3$"
         end            
-        tit = "pressure, "*temp
+        tit   = "pressure, "*temp
+        cb    = :viridis
+        cblim = (minimum(d),maximum(d))
     elseif type == "epII"
-        d = mpD.ϵpII
-        lab = L"$\epsilon_{\mathrm{II}}^{\mathrm{acc}}$"
-        tit = "plastic strain, "*temp
+        d     = mpD.ϵpII
+        lab   = L"$\epsilon_{\mathrm{II}}^{\mathrm{acc}}$"
+        tit   = "plastic strain, "*temp
+        cb    = :viridis
+        cblim = (0.0,maximum(d))
     elseif type == "epV"
-        d = mpD.ϵpV
-        lab = L"$\epsilon_{p}^{\mathrm{vol}}$"
-        tit = "volumetric plastic strain, "*temp
+        d     = mpD.ϵpV
+        lab   = L"$\epsilon_{p}^{\mathrm{vol}}$"
+        tit   = "volumetric plastic strain, "*temp
+        cb    = :seismic
+        if minimum(d) == maximum(d)
+            cblim = (-1.0,1.0)
+        else
+            cblim = (-maximum(abs.(d)),maximum(abs.(d)))
+        end
     elseif type == "du"
         d = sqrt.(mpD.u[:,1].^2+mpD.u[:,2].^2)
         lab = L"$\Delta u$"
         tit = "displacement, "*temp
+        cb  = :viridis
+        cblim = (0.0,maximum((d)))
     else
         err_msg = "$(type): plot option undefined"
         throw(error(err_msg))
@@ -93,10 +105,11 @@ end
     xlabel = L"$x-$direction [m]",
     ylabel = L"$z-$direction [m]",
     label  = lab,
+    c      = cb,
+    clim   = cblim,
+    ylim   = (-10.0,20.0),
+    title  = tit,
     aspect_ratio=1,
-    c=:viridis,
-    ylim=(-10.0,20.0),
-    title=tit,
     )
     display(plot(p1;layout=(1,1),size=(500,250))) 
     return ctr+=1
