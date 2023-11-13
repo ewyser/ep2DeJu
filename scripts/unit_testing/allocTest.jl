1ea2ce7dd92b8b54821cd2fc4db88f083014b3d9
ewyser <manuwyser@gmail.com>
2023-11-13 20:51:48 +0100

    update ``allocTest.jl``

--- scripts/unit_testing/allocTest.jl
+++ scripts/unit_testing/allocTest.jl
@@ -3,20 +3,22 @@
 # include dependencies
 include("../../src/superInclude.jl")
 using BenchmarkTools
-@views function allocCheck(nel::Int64,varPlot::String,cmType::String; kwargs...)
+@views function allocCheck(L::Vector{Float64},nel::Int64,varPlot::String,cmType::String; kwargs...)
     ϕ∂ϕType,fwrkDeform,trsfrAp,isΔFbar,isGRF = getKwargs(kwargs)
-    @info "** ϵp2-3De v$(getVersion()): time & allocation evaluation **"
+    @info "** ϵp$(length(L))De v$(getVersion()): $(fwrkDeform) strain formulation **"
+    @info "init..."
+    # mesh setup
+    meD     = meshSetup(nel,L,typeD)                                            # mesh geometry setup
     # independant physical constant
     g       = 9.81                                                              # gravitationnal acceleration [m/s^2]            
-    K,G,Del = D(1.0e6,0.3,2)                                                    # elastic matrix D(E,ν) Young's mod. [Pa] + Poisson's ratio [-]    
+    E,ν     = 1.0e6,0.3                                                         # Young's mod. [Pa], Poisson's ratio [-]
+    K,G,Del = D(E,ν,meD.nD)                                                     # elastic matrix D(E,ν) Young's mod. [Pa] + Poisson's ratio [-]    
     ρ0      = 2700.0                                                            # density [kg/m^3]
     yd      = sqrt((K+4.0/3.0*G)/ρ0)                                            # elastic wave speed [m/s]
     c0,cr   = 20.0e3,4.0e3                                                      # cohesion [Pa]
     ϕ0,ϕr,ψ0= 20.0*π/180,7.5*π/180,0.0                                          # friction angle [Rad], dilation angle [Rad]                                                              
     t,te,tg = 15.0,10.0,15.0/1.5                                                # simulation time [s], elastic loading [s], gravity load
-    # mesh & mp setup
-    L       = [64.1584,12.80]                                                   # domain geometry
-    meD     = meshSetup(nel,L,typeD)                                            # mesh geometry setup
+    # mp setup
     mpD     = pointSetup(meD,L,c0,cr,ϕ0,ϕr,ρ0,isGRF,typeD)                      # material point geometry setup
     Hp      = -60.0e3*meD.h[1]                                                  # softening modulus
     # constitutive model param.
@@ -45,4 +47,4 @@ using BenchmarkTools
     @btime elast!($mpD,$cmParam.Del,$fwrkDeform)
     return msg("(✓) Done! exiting...")
 end
-allocCheck(40,"P","MC";shpfun=:bsmpm,fwrk=:finite,trsf=:mUSL,vollock=true)
+allocCheck([64.1584,64.1584/4,12.80],80,"P","MC";shpfun=:bsmpm,fwrk=:infinitesimal,trsf=:mUSL,vollock=true)
