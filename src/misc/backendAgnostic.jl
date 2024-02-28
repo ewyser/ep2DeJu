a5b47ecaaedc2245c5aaefccf4a647580ed22e02
Manu <manuwyser@gmail.com>
2024-02-28 11:26:37 +0100

    misc

--- src/misc/backendAgnostic.jl
+++ src/misc/backendAgnostic.jl
@@ -1,12 +1,18 @@
 # https://juliagpu.github.io/KernelAbstractions.jl/stable/
 # include("./src/misc/backendAgnostic.jl")
-using KernelAbstractions, CUDA
+using KernelAbstractions, CUDA, LinearAlgebra
 
 
 @kernel inbounds=true function kernel!(A,nx,ny)
     i,j = @index(Global, NTuple)
     if 1<i<size(A,1) && 1<j<size(A,2)
         A[i,j] = 2.0 * A[i,j]
+    end
+end
+@kernel inbounds=true function test!(sig,np)
+    k = @index(Global)
+    if k<=np
+        λ,n = eigen(sig[:,:,k],sortby=nothing)
     end
 end
 
@@ -36,3 +42,12 @@ end
 
 # print result
 #println(A)
+
+
+
+sig = ones(3,3,10)
+sig_D = sig
+global eigN! = test!(CPU(), KernelAbstractions.isgpu(dev) ? 256 : 1024)
+np = 10
+eigN!(sig_D,np, ndrange=np)
+KernelAbstractions.synchronize(dev)
