f1767f48cd38a64552c20da71275f5d450dcd200
ewyser <manuwyser@gmail.com>
2024-03-02 17:50:46 +0100

    parallel execution for ``DM``procedure

--- src/fun_fs/mapsto.jl
+++ src/fun_fs/mapsto.jl
@@ -119,31 +119,50 @@ end
     end
     return nothing
 end
-@views function DM!(mpD,meD,Δt)
+
+@kernel inbounds = true function kernel_momentum(mpD,meD)
+    p = @index(Global)
+    for dim ∈ 1:meD.nD
+        if p≤mpD.nmp 
+            # accumulation
+            for nn ∈ 1:meD.nn
+                @atom meD.pn[  mpD.p2n[nn,p],dim]+= mpD.ϕ∂ϕ[nn,p,1]*(mpD.m[p]*mpD.v[p,dim])
+            end
+        end
+    end
+end
+@kernel inbounds = true function kernel_velocity(meD)
+    n = @index(Global)
+    for dim ∈ 1:meD.nD
+        if n≤meD.nno[end] 
+            if meD.mn[n]>0.0
+                meD.vn[n,dim] = (meD.pn[n,dim]*(1.0/meD.mn[n])*meD.bc[n,dim])
+            end   
+        end
+    end
+end
+@kernel inbounds = true function kernel_displacement(mpD,meD,Δt)
+    p = @index(Global)
+    for dim ∈ 1:meD.nD
+        if p≤mpD.nmp 
+            # accumulation
+            mpD.u[p,dim]+= Δt*(mpD.ϕ∂ϕ[:,p,1]'*meD.vn[mpD.p2n[:,p],dim])
+        end
+    end
+end
+function DM!(mpD,meD,Δt)
     # initialize for DM
     meD.pn.= 0.0
     meD.vn.= 0.0
     # accumulate material point contributions
-    for dim ∈ 1:meD.nD
-        # accumulation
-        for p ∈ 1:mpD.nmp
-            meD.pn[mpD.p2n[:,p],dim].+= mpD.ϕ∂ϕ[:,p,1].*(mpD.m[p].*mpD.v[p,dim])
-        end
-    end    
+    @isdefined(DMp2nK!) ? nothing : DMp2nK! = kernel_momentum(CPU())
+    DMp2nK!(mpD,meD; ndrange=mpD.nmp);sync(CPU())
     # solve for nodal incremental displacement
-    for dim ∈ 1:meD.nD
-        for n ∈ 1:meD.nno[end]
-            if meD.mn[n]>0.0
-                meD.vn[n,dim] = (meD.pn[n,dim]*(1.0/meD.mn[n])*meD.bc[n,dim])
-            end    
-        end
-    end
+    @isdefined(DMsolveK!) ? nothing : DMsolveK! = kernel_velocity(CPU())
+    DMsolveK!(meD; ndrange=meD.nno[end]);sync(CPU())
     # update material point's displacement
-    for dim ∈ 1:meD.nD
-        for p ∈ 1:mpD.nmp
-            mpD.u[p,dim]+= Δt*(mpD.ϕ∂ϕ[:,p,1]'*meD.vn[mpD.p2n[:,p],dim])
-        end        
-    end
+    @isdefined(DMdisplK!) ? nothing : DMdisplK! = kernel_displacement(CPU())
+    DMdisplK!(mpD,meD,Δt; ndrange=mpD.nmp);sync(CPU())
     return nothing
 end
 #--------------------------------------------------------------------------------------------
